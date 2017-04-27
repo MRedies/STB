309ad2cd1c3c5d9d88f2c0ad9af1a7a7ac350563
Matthias Redies <matthias.redies@posteo.de>
2017-04-27 21:52:19 +0200

    unit_class cell

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,12 @@
 program STB
-    write(*,*) "Blalal"
+    use Class_unit_cell
+    implicit none
+    integer(4)      :: num  
+    type(unit_cell) :: bla
+    bla =  unit_cell(3,2.3,4.5)
+    num =  bla%get_num_atom()
+    
+    write(*,*) "Num: ", num 
+    
 end program STB
 
--- src/unit_cell.f90
+++ src/unit_cell.f90
@@ -1,14 +1,34 @@
 module Class_unit_cell
     implicit none
     private
+    public: : unit_cell, get_num_atom
 
-    type, public        :: unit_cell
+    type unit_cell
         ! number of non-redundant atoms pre unit cell
-        integer*4       :: num_atoms
-        real*8          :: phi, theta
-        integer*4(3)    :: neighbours
-        real*8(2)       :: pos
+        integer(4)                  :: num_atoms, hex_size
+        real(8)                     :: phi, theta
+        integer(4), dimension(3)    :: neighbours
+        real(8),    dimension(3)    :: pos
     contains
+        procedure :: get_num_atoms =>  get_num_atoms
     end type unit_cell
 contains
+        function get_num_atoms(this) result(num)
+            Class(unit_cell), intent(in) :: this
+            integer(4) :: num
+            num = this%num_atoms 
+        end function get_num_atoms
+
+        function calc_num_atoms(hex_size) result(num_atoms)
+            integer(4), intent(in)       :: hex_size
+            integer(4), intent(out)      :: num_atoms
+
+            if (hex_size /= 1) then
+                num_atoms =  3 *  hex_size**2 -  5 *  hex_size +  2
+            else
+                num_atoms =  1
+            end if
+        end function calc_num_atoms
+
+end module
 
