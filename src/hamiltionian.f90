676f952eb62907fb7302c82929917c7dd1fa49d9
Matthias Redies <matthias.redies@posteo.de>
2017-05-31 16:58:18 +0200

    analytic derivaties del_k_i H works

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -3,7 +3,8 @@ module Class_hamiltionian
     use output
     use Class_unit_cell
     use m_npy
-    USE, INTRINSIC :: IEEE_ARITHMETIC
+    use lapack95
+    use f95_precision
     implicit none
     
     type hamil
@@ -12,15 +13,69 @@ module Class_hamiltionian
         real(8)         :: I !> stoner parameter
         type(unit_cell) :: UC !> unit cell
     contains
-        procedure :: setup_H             => setup_H
-        procedure :: calc_eigenvalues    => calc_eigenvalues
-        procedure :: set_EigenE          => set_EigenE
-        procedure :: set_hopping         => set_hopping
-        procedure :: set_Stoner          => set_Stoner
-        procedure :: setup_Stoner_mtx    => setup_Stoner_mtx
+        procedure :: setup_H                => setup_H
+        procedure :: calc_eigenvalues       => calc_eigenvalues
+        procedure :: set_EigenE             => set_EigenE
+        procedure :: set_hopping            => set_hopping
+        procedure :: set_Stoner             => set_Stoner
+        procedure :: setup_Stoner_mtx       => setup_Stoner_mtx
+        procedure :: set_derivative_k       => set_derivative_k
+        procedure :: calc_deriv_elem        => calc_deriv_elem
+        procedure :: calc_berry_tensor_elem => calc_berry_tensor_elem
+        procedure :: calc_berry_z           => calc_berry_z
+        procedure :: compare_derivative     => compare_derivative
     end type hamil
 
 contains
+    subroutine compare_derivative(self, k, d_k)
+        implicit none 
+        class(hamil), intent(in)    :: self
+        real(8), intent(in)         :: k(3), d_k
+        real(8)                     :: new_k(3)
+        complex(8), allocatable     :: fd_H(:,:), H_back(:,:), H_forw(:,:), del_H(:,:)
+        integer(4)                  :: N, k_idx 
+        
+        N = 2 * self%UC%num_atoms
+        allocate(H_back(N,N))
+        allocate(H_forw(N,N))
+        allocate(fd_H(N,N))
+        allocate(del_H(N,N))
+       
+        do k_idx =  1,2
+            new_k = k
+            new_k(k_idx) = k(k_idx) - 0.5d0 * d_k
+            call self%setup_H(new_k, H_back)
+
+            new_k(k_idx) = k(k_idx) + 0.5d0 * d_k
+            call self%setup_H(new_k, H_forw)
+            fd_H =  (H_forw -  H_back)/d_k
+
+            call self%set_derivative_k(k, k_idx, del_H)
+            
+            !write (*,*) "Forw:"
+            !call print_mtx(H_forw)
+            !write (*,*) "Backw:"
+            !call print_mtx(H_back)
+            
+            !write (*,*) "k =", k
+            !write (*,*) "k_idx =", k_idx
+            !write (*,*) "FD: "
+            !call print_mtx(fd_H)
+            !write (*,*) "del_H:"
+            !call print_mtx(del_H)
+            !write (*,*) "Matrix norm: real", norm2(real(fd_H - del_H))
+            !write (*,*) "Matrix norm: imag", norm2(aimag(fd_H - del_H))
+            if(norm2(real(fd_H - del_H)) >= 1d-8 .or. &
+                norm2(aimag(fd_H - del_H)) >= 1d-8) then
+                write (*,*) "mist"
+                stop
+            endif
+
+        enddo
+
+
+    end subroutine compare_derivative
+
     subroutine setup_H(self,k,H)
         implicit none
         class(hamil)        :: self
@@ -141,7 +196,156 @@ contains
         enddo
 
     end subroutine set_hopping
+
+    subroutine set_derivative_k(self, k, k_idx, del_H)
+        implicit none
+        class(hamil), intent(in)  :: self
+        integer(4), intent(in)    :: k_idx
+        real(8), intent(in)       :: k(3)
+        complex(8), allocatable   :: del_H(:,:)
+        real(8)                   :: r(3), k_dot_r
+        complex(8)                :: forw, back
+        integer(4)                :: i, j, conn, i_d, j_d
+
+        if(k(3) /= 0) then
+            write (*,*) "K_z not zero in set_derivative_k"
+            stop
+        endif
     
+        del_H = 0d0
+        do i = 1,self%UC%num_atoms
+            i_d =  i + self%UC%num_atoms
+            do conn = 1,self%UC%atoms(i)%n_neigh
+
+                j   = self%UC%atoms(i)%neigh_idx(conn)
+                j_d = j + self%UC%num_atoms
+                r   = self%UC%atoms(i)%neigh_conn(conn,:)
+
+                k_dot_r = dot_product(k, r)
+                forw    = i_unit * r(k_idx) * self%UC%atoms(i)%hopping(conn) &
+                          * exp(i_unit * k_dot_r)
+                
+                r       = - r
+                k_dot_r = - k_dot_r
+                back    = i_unit * r(k_idx) * self%UC%atoms(i)%hopping(conn) &
+                          * exp(i_unit * k_dot_r)
+
+                !Spin up
+                del_H(i,j)     = del_H(i,j) + forw 
+                del_H(j,i)     = del_H(j,i) + back
+                !Spin down
+                del_H(i_d,j_d) = del_H(i_d,j_d) + forw
+                del_H(j_d,i_d) = del_H(j_d,i_d) + back
+            enddo
+        enddo
+    end subroutine
+
+    function calc_deriv_elem(self, psi_nk, psi_mk, k, k_idx) result(elem)
+        implicit none
+        class(hamil), intent(in)   :: self
+        complex(8), intent(in)     :: psi_nk(:), psi_mk(:)
+        real(8), intent(in)        :: k(3)
+        integer(4), intent(in)     :: k_idx
+        complex(8)                 :: elem
+        complex(8), allocatable    :: del_H(:,:), tmp_vec(:)
+        integer(4)                 :: n
+
+        n =  size(psi_nk)
+        allocate(tmp_vec(n))
+        allocate(del_H(n,n))
+        
+        call self%set_derivative_k(k, k_idx, del_H)
+        tmp_vec =  matmul(del_H, psi_mk)
+        elem =  dot_product(conjg(psi_nk), tmp_vec)
+        
+        deallocate(tmp_vec)
+        deallocate(del_H)
+    end function
+
+    subroutine calc_berry_tensor_elem(self, k_i, k_j, k, omega, eig_val)
+        implicit none 
+        class(hamil), intent(in)           :: self
+        integer(4), intent(in)             :: k_i, k_j
+        real(8), intent(in)                :: k(3)
+        real(8), allocatable               :: omega(:) !> \f$ \Omega_{ij}^n\f$
+        real(8), allocatable               :: eig_val(:), rwork(:)
+        complex(8), allocatable            :: H(:,:), del_H(:,:), work(:)
+        complex(8)                         :: summe, term
+        complex(8)  :: fac
+        integer(4), allocatable :: iwork(:) 
+        integer(4)  :: n_dim, n, m, info, lwork, lrwork, liwork
+
+        n_dim = 2 * self%UC%num_atoms
+        allocate(H(n_dim,n_dim))
+        allocate(del_H(n_dim,n_dim))
+        
+        if(.not. allocated(eig_val)) then
+            allocate(eig_val(n_dim))
+        endif
+        if(.not. allocated(omega))then
+            allocate(omega(n_dim))
+        endif
+
+        H = 0d0
+        call self%setup_H(k, H)
+        lwork  = 4 * n_dim**2 +  2*n_dim 
+        lrwork = 8 * n_dim**2 +  5*n_dim + 1
+        liwork = 10* n_dim +  3
+        allocate(work(lwork))
+        allocate(rwork(lrwork))
+        allocate(iwork(liwork))
+        call zheevd('V', 'L', n_dim, H, n_dim, eig_val, &
+                    work, lwork, rwork, lrwork, iwork, liwork, info)
+        if(info /= 0) then
+            write (*,*) "ZHEEVD in berry calculation failed"
+        endif
+        !call heevd(H, eig_val, 'V')
+
+        do n = 1,n_dim
+            summe = 0d0
+            do m = 1,n_dim
+                if(n /= m) then
+                    fac = 1d0 / ((eig_val(n) - eig_val(m))**2 +  i_unit * 1d-6)
+                    !write (*,*) "Matrix H:"
+                    !call print_mtx(H)
+                    !write (*,*) "k_i", k_i
+                    !write (*,*) "k_j", k_j
+
+                    term =        self%calc_deriv_elem(H(:,n), H(:,m), k, k_i)
+                    !write (*,*) "Term 1: ", term
+                    term = term * self%calc_deriv_elem(H(:,m), H(:,n), k, k_j)
+                    !write (*,*) "Term 2: ", self%calc_deriv_elem(H(:,m), H(:,n), k, k_j)
+                    summe =  summe +  fac * term
+                endif
+            enddo
+            omega(n) = -2d0 * aimag(summe)
+        enddo
+
+        deallocate(H)
+        deallocate(del_H)
+    end subroutine calc_berry_tensor_elem
+
+    subroutine calc_berry_z(self,k, z_comp, eig_val)
+        implicit none
+        class(hamil), intent(in)            :: self
+        real(8), intent(in)                 :: k(3)
+        real(8), allocatable                :: z_comp(:) !> \f$ \Omega^n_z \f$
+        real(8), allocatable                :: eig_val(:)
+        real(8), allocatable                :: tmp(:)
+
+        call self%calc_berry_tensor_elem(1,2,k, tmp, eig_val)
+        
+        if(.not. allocated(z_comp)) then
+            allocate(z_comp(size(tmp)))
+        endif
+
+        z_comp = 0.5d0 *  tmp
+        call self%calc_berry_tensor_elem(2,1,k,tmp, eig_val)
+        z_comp = - 0.5d0 * tmp
+
+        deallocate(tmp)
+    end subroutine calc_berry_z
+
     Subroutine  calc_eigenvalues(self, k_list, eig_val)
         Implicit None
         class(hamil)                      :: self
