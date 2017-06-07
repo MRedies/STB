f053a09e137e17176c5b90bbeed5256d7c55ee0f
Matthias Redies <matthias.redies@posteo.de>
2017-06-07 16:16:13 +0200

    added sqrt to cnorm

--- src/helper.f90
+++ src/helper.f90
@@ -9,13 +9,18 @@ contains
         implicit none
         complex(8), intent(in)   :: vec(:)
         real(8)                  :: norm
-        integer(4)               :: i
-
-        norm =  0d0 
-        do i =  1,size(vec)
-            norm =  norm +  vec(i) * conjg(vec(i))
-        enddo
+    
+        ! see fortran defition for complex dot_product
+        norm = sqrt(dot_product(vec,vec))
     end function cnorm2
+
+    function my_norm2(vec) result(norm)
+        implicit none
+        real(8), intent(in) :: vec(:)
+        real(8)             :: norm
+
+        norm = sqrt(dot_product(vec,vec))
+    end function my_norm2
 
     subroutine my_section(me, nProcs, length, first, last)
         implicit none
