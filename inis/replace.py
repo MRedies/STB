61a0348dfce1054f2d737e4baf1e186d8d23eb57
Matthias Redies <matthias.redies@posteo.de>
2017-08-03 15:26:09 +0200

    inis

--- inis/replace.py
+++ inis/replace.py
@@ -2,16 +2,16 @@ import numpy as np
 
 
 
-#atan = np.linspace(0.01,10, 200)
-atan = np.linspace(0.8,0.9,10)
-#kpd = np.arange(2,100)
+atan = np.linspace(0.0,0.4, 10)
+#atan = np.logspace(-2, 1.0,12)
+#rad = np.arange(21,60,3)
 prefix = "./inis/"
 
 cnt = 0
 for i in atan:
-    with open(prefix+"example.cfg", "rt") as fin:
-        with open(prefix + "atan_{}.cfg".format(cnt), "wt") as fout:
+    with open(prefix + "example.cfg", "rt") as fin:
+        with open(prefix + "rand_scan_{}.cfg".format(cnt), "wt") as fout:
             for line in fin:
-                #fout.write(line.replace('%kpd%', "{:04d}".format(i)))
-                fout.write(line.replace('%atan%', "{:011.6f}".format(i)))
+                #fout.write(line.replace('%rad%', "{:04d}".format(i)))
+                fout.write(line.replace('%rand%', "{:011.6f}".format(i)))
     cnt +=1
