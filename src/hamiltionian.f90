ed9f3c5e4dfd35d4efcba030deb7342fca370c00
Jonathan  Kipp <j.kipp@fz-juelich.de>
2019-09-10 09:52:04 +0200

    collinear perturbation test

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1167,14 +1167,19 @@ contains
                j =  self%UC%atoms(i)%neigh_idx(conn)
                j_d = j + self%num_up
                ! H_xc_at = lambda * (H_0 + H_1 * t_nc)
+               !testing collinear perturbation here
                temp(i,i)     =  self%lambda*(cos(theta_col)-sin(theta_col)*theta_nc/2d0)
                temp(i_d,i_d) = -self%lambda*(cos(theta_col)-sin(theta_col)*theta_nc/2d0)
-               temp(j,j)     =  self%lambda*(cos(theta_col)+sin(theta_col)*theta_nc/2d0)
-               temp(j_d,j_d) = -self%lambda*(cos(theta_col)+sin(theta_col)*theta_nc/2d0)
+               !temp(j,j)     =  self%lambda*(cos(theta_col)+sin(theta_col)*theta_nc/2d0)
+               temp(j,j)     =  self%lambda*(cos(theta_col)-sin(theta_col)*theta_nc/2d0)
+               !temp(j_d,j_d) = -self%lambda*(cos(theta_col)+sin(theta_col)*theta_nc/2d0)
+               temp(j_d,j_d) = -self%lambda*(cos(theta_col)-sin(theta_col)*theta_nc/2d0)
                temp(i,i_d)   =  self%lambda*(sin(theta_col)+cos(theta_col)*theta_nc/2d0)*exp(-i_unit*(phi_col+phi_nc/2d0))
                temp(i_d,i)   =  self%lambda*(sin(theta_col)+cos(theta_col)*theta_nc/2d0)*exp( i_unit*(phi_col+phi_nc/2d0))
-               temp(j,j_d)   =  self%lambda*(sin(theta_col)-cos(theta_col)*theta_nc/2d0)*exp(-i_unit*(phi_col-phi_nc/2d0))
-               temp(j_d,j)   =  self%lambda*(sin(theta_col)-cos(theta_col)*theta_nc/2d0)*exp( i_unit*(phi_col-phi_nc/2d0))
+               !temp(j,j_d)   =  self%lambda*(sin(theta_col)-cos(theta_col)*theta_nc/2d0)*exp(-i_unit*(phi_col-phi_nc/2d0))
+               temp(j,j_d)   =  self%lambda*(sin(theta_col)+cos(theta_col)*theta_nc/2d0)*exp(-i_unit*(phi_col-phi_nc/2d0))
+               !temp(j_d,j)   =  self%lambda*(sin(theta_col)-cos(theta_col)*theta_nc/2d0)*exp( i_unit*(phi_col-phi_nc/2d0))
+               temp(j_d,j)   =  self%lambda*(sin(theta_col)+cos(theta_col)*theta_nc/2d0)*exp( i_unit*(phi_col-phi_nc/2d0))
             endif
          enddo
       enddo
