a7a1ff90e8e8568a09671f0b6b21560b2efd1caf
Jonathan  Kipp <j.kipp@fz-juelich.de>
2019-10-17 13:54:21 +0200

    test deallocation

--- src/k_space.F90
+++ src/k_space.F90
@@ -924,7 +924,7 @@ contains
 
       err =  0
       allocate(eig_val_new(2*num_up, last-first+1), stat=err(1))
-      if(self%calc_hall)   allocate(omega_z_new(2*num_up, last-first+1), stat=err(2))
+      !if(self%calc_hall)   allocate(omega_z_new(2*num_up, last-first+1), stat=err(2))
       if(self%calc_orbmag) allocate(Q_L_new(n_ferm,        last-first+1), stat=err(3))
       if(self%calc_orbmag) allocate(Q_IC_new(n_ferm,        last-first+1), stat=err(3))
 
@@ -940,11 +940,14 @@ contains
                call self%ham%calc_berry_z(omega_z_new(:,cnt),&
                                        eig_val_new(:,cnt), del_kx, del_ky)
                do pert_idx=1,4
+                  write(*) "before"
                   if(allocated(del_kx)) deallocate(del_kx)
                   if(allocated(del_ky)) deallocate(del_ky)
                   if(allocated(omega_z_pert_new)) deallocate(omega_z_pert_new)
                   allocate(omega_z_new(2*num_up, last-first+1), stat=err(2))
+                  write(*) "before2"
                   call self%ham%calc_eig_and_velo(k, eig_val_new(:,cnt), del_kx, del_ky,pert_idx)
+                  write(*) "after"
                   call self%ham%calc_berry_z(omega_z_pert_new(:,cnt),&
                                              eig_val_new(:,cnt), del_kx, del_ky)
                   omega_z_new(:,cnt) = omega_z_new(:,cnt) + omega_z_pert_new(:,cnt)
