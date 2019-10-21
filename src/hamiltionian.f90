123a088a393bee8a4603e62a936ca7fb79f2581e
Jonathan  Kipp <j.kipp@fz-juelich.de>
2019-10-21 09:13:40 +0200

    swapped E_T H_xc E to E H_xc H_T

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1195,18 +1195,18 @@ contains
          enddo
       enddo
       !rotate hxc into the eigenbasis of the hamiltonian with E_dagger H E
-      call zgemm('N', 'N', n_dim, n_dim, n_dim, &
+      call zgemm('N', 'C', n_dim, n_dim, n_dim, &
                   c_1, temp, n_dim,&
                   eig_vec_mtx, n_dim,&
                   c_0, ret, n_dim)
-      call zgemm('C', 'N', n_dim, n_dim, n_dim, &
+      call zgemm('N', 'N', n_dim, n_dim, n_dim, &
                   c_1, eig_vec_mtx, n_dim,&
                   ret, n_dim,&
                   c_0, H_temp, n_dim)
       do i=1,n_dim
          do j=1,n_dim
             if(i /= j) then
-               dE = eig_val(i)-eig_val(j)
+               dE = eig_val(j)-eig_val(i)
                   if       (abs(dE)>10**(-8))   then
                      H_temp(i,j)=H_temp(i,j)/dE
                   else if  (abs(dE)<=10**(-8))  then
