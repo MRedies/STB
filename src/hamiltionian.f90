6ac093e9925dff72947252c6aa1386fb0fed62fa
Jonathan  Kipp <j.kipp@fz-juelich.de>
2019-10-24 15:38:24 +0200

    there is no conversion from hartree to eV needed

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1211,7 +1211,7 @@ contains
       do i=1,n_dim
          do j=1,n_dim
             if(i /= j) then
-               dE = (eig_val(j)-eig_val(i))*self%units%energy
+               dE = (eig_val(j)-eig_val(i))!*self%units%energy
                   if       (abs(dE)>10**(-8))   then
                      H_temp(i,j)=H_temp(i,j)/dE
                   else if  (abs(dE)<=10**(-8))  then
