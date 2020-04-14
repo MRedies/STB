a6781cb1dbef1b7ec6baa24bfa08d781b2664f9f
JonathanKipp <j.kipp@fz-juelich.de>
2020-04-14 17:14:33 +0200

    print state ksp

--- src/k_space.F90
+++ src/k_space.F90
@@ -945,7 +945,7 @@ contains
                omega_z_pert_new=0d0
                !allocation for omega_z_pert_new can be done here, since ham%calc_berry_z sets to zero
                do pert_idx=1,4
-                  if(root == me) write (*,*) "calc_new_berrypoints", pert_log, pert_idx
+                  write (*,*) "calc_new_berrypoints", me, pert_log, pert_idx
                   if(allocated(del_kx)) deallocate(del_kx)
                   if(allocated(del_ky)) deallocate(del_ky)
                   call self%ham%calc_eig_and_velo(k, eig_val_new(:,cnt), del_kx, del_ky,pert_idx)
