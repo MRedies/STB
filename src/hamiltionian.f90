f73f2f8f33c611516de15b865a215d9ae1e663c5
JonathanKipp <j.kipp@fz-juelich.de>
2020-06-30 11:45:44 +0200

    pert

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1179,12 +1179,12 @@ contains
         write(*,*) "full"
         temp(i,i)     =  fac*(cos(theta_col + theta_nc/2d0) - cos(theta_col))
         temp(i_d,i_d) = -fac*(cos(theta_col + theta_nc/2d0) - cos(theta_col))
-        temp(j,j)     = -fac*(cos(theta_col - theta_nc/2d0) - cos(theta_col))
-        temp(j_d,j_d) =  fac*(cos(theta_col - theta_nc/2d0) - cos(theta_col))
+        temp(j,j)     =  fac*(cos(theta_col - theta_nc/2d0) - cos(theta_col))
+        temp(j_d,j_d) = -fac*(cos(theta_col - theta_nc/2d0) - cos(theta_col))
         temp(i,i_d)   =  fac*(sin(theta_col + theta_nc/2d0) - sin(theta_col))*exp(-i_unit*(phi_col+phi_nc/2d0))
         temp(i_d,i)   =  fac*(sin(theta_col + theta_nc/2d0) - sin(theta_col))*exp( i_unit*(phi_col+phi_nc/2d0))
-        temp(j,j_d)   = -fac*(sin(theta_col - theta_nc/2d0) - sin(theta_col))*exp(-i_unit*(phi_col-phi_nc/2d0))
-        temp(j_d,j)   = -fac*(sin(theta_col - theta_nc/2d0) - sin(theta_col))*exp( i_unit*(phi_col-phi_nc/2d0))
+        temp(j,j_d)   =  fac*(sin(theta_col - theta_nc/2d0) - sin(theta_col))*exp(-i_unit*(phi_col-phi_nc/2d0))
+        temp(j_d,j)   =  fac*(sin(theta_col - theta_nc/2d0) - sin(theta_col))*exp( i_unit*(phi_col-phi_nc/2d0))
       else
         write(*,*) "linear"
         temp(i,i)     = -fac*sin(theta_col)*theta_nc/2d0
