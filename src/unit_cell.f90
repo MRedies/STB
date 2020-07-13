8ee33d0d20498e3a2d2f69b599fa8fc8e3ac8417
JonathanKipp <j.kipp@fz-juelich.de>
2020-07-13 14:21:24 +0200

    spiral

--- src/unit_cell.f90
+++ src/unit_cell.f90
@@ -941,7 +941,6 @@ contains
                     m = self%m0_B
                 endif
             endif
-            write(*,*) "End: ", m
             call self%atoms(i)%set_m_cart(m(1), m(2), m(3))
         enddo
     end subroutine set_mag_linrot_1D_spiral
