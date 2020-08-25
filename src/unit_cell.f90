f317f047b1deac9a12f0749b19746a9f602fffed
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-08-25 16:31:19 +0200

    spiral

--- src/unit_cell.f90
+++ src/unit_cell.f90
@@ -486,7 +486,7 @@ contains
       allocate (site_type(self%num_atoms))
       posA = conn_mtx(3, :)
       posB = -conn_mtx(2, :)
-      pos = 0d0
+      pos = -self%num_atoms/2d0*conn_vec_1
       do i = 1, self%num_atoms
          if (mod(i - 1, 2) == 0) then
             line(i, :) = posA + pos
