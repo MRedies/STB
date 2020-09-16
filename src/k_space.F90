8d5f6acd3b9ccabd6da6d16d197a751c4297ddbf
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-09-16 10:37:00 +0200

    Revert "macbook" spiral This reverts commit 8b3930491dcccfdbcdfb184cd41d743437d3b0fe.

--- src/k_space.F90
+++ src/k_space.F90
@@ -276,8 +276,7 @@ contains
       elseif(trim(self%ham%UC%uc_type) == "honey_2d") then
          call self%setup_inte_grid_hex(self%DOS_num_k_pts)
       elseif(trim(self%ham%UC%uc_type) == "honey_line") then
-         call self%setup_inte_grid_hex(self%DOS_num_k_pts)
-         !call self%setup_inte_grid_para(self%DOS_num_k_pts)
+         call self%setup_inte_grid_para(self%DOS_num_k_pts)
       else
          call error_msg("DOS k-grid not known", abort=.True.)
       endif
@@ -1574,8 +1573,7 @@ contains
       elseif(trim(self%ham%UC%uc_type) == "honey_2d") then
          call self%setup_inte_grid_hex(self%berry_num_k_pts)
       elseif(trim(self%ham%UC%uc_type) == "honey_line") then
-         call self%setup_inte_grid_hex(self%berry_num_k_pts)
-         !call self%setup_inte_grid_para(self%berry_num_k_pts)
+         call self%setup_inte_grid_para(self%berry_num_k_pts)
       else
          call error_msg("berry k-grid not known", abort=.True.)
       endif
