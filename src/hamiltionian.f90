78170d7aeb6a977601544f58e07dae28a6126fea
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-09-28 15:45:58 +0200

    macbook

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1354,7 +1354,7 @@ contains
       allocate(rwork(lrwork), stat=ierr(2))
       allocate(iwork(liwork), stat=ierr(3))
       call check_ierr(ierr, me_in=self%me, msg=[" tried to allocate in zheevd"])
-      save_npy(folder//"hamiltonian.npy",eig_vec)
+      call save_npy(folder//"hamiltonian.npy",eig_vec)
       call zheevd('V', 'L', n_dim, eig_vec, n_dim, eig_val, &
                   work, lwork, rwork, lrwork, iwork, liwork, info)
       if(info /= 0) then
