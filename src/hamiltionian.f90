1f5f70d5e318a8241ef1edf8fbfbcec2d035e98f
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-10-01 14:59:10 +0200

    diag cont surf and sea

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1462,7 +1462,7 @@ contains
                fac =  gamma/(dE*(eig_val(m)**2+gamma**2))&
                       - dE**2/(dE**2 + eta_sq)**2*log(numer&
                                            /denom)
-               z_comp(n) = z_comp(n) - 1d0/Pi &
+               z_comp(n) = z_comp(n) + 1d0/Pi &
                            * aimag(fac * x_mtx(n,m) * x_mtx(m,n))
             endif
          enddo
