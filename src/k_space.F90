d339ad85ec09359599a18be9655b21ec3b2bcbaf
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-10-12 12:16:29 +0200

    diag cont surf and sea

--- src/k_space.F90
+++ src/k_space.F90
@@ -960,9 +960,6 @@ contains
       call my_section(self%me, self%nProcs, N_k, first, last)
 
       err =  0
-      if (self%me==root) then
-         write(*,*) "FLAG ksp%calc_new_berry()"
-      endif
       allocate(eig_val_new(2*num_up, last-first+1), stat=err(1))
       if(self%calc_hall)   allocate(omega_z_new(2*num_up, last-first+1), stat=err(2))
       if(self%calc_hall_diag)   allocate(omega_surf_new(n_ferm, last-first+1), stat=err(3))
@@ -970,7 +967,7 @@ contains
       if(self%calc_orbmag) allocate(Q_L_new(n_ferm,        last-first+1), stat=err(5))
       if(self%calc_orbmag) allocate(Q_IC_new(n_ferm,        last-first+1), stat=err(6))
       if (self%me==root) then
-         write(*,*) "FLAG 2 ksp%calc_new_berry()"
+         write(*,*) "FLAG ksp%calc_new_berry()"
       endif
       call check_ierr(err, self%me, " new chunk alloc")
       call MPI_Comm_rank(MPI_COMM_WORLD, me, ierr)
@@ -1007,7 +1004,9 @@ contains
          do k_idx = first, last
             k = self%new_k_pts(:,k_idx)
             call self%ham%calc_eig_and_velo(k, eig_val_new(:,cnt), del_kx, del_ky,0)
-         
+            if (self%me==root) then
+               write(*,*) "FLAG 2 ksp%calc_new_berry()"
+            endif
             if(self%calc_hall) then
                call self%ham%calc_berry_z(omega_z_new(:,cnt),&
                                        eig_val_new(:,cnt), del_kx, del_ky)
@@ -1242,7 +1241,7 @@ contains
       integer   , intent(in)  :: kidx_all(:)
       real(8), intent(in)     :: omega_z_all(:,:)
       real(8), allocatable    :: hall(:)
-      integer                 :: loc_idx, n, m, n_hall, k_idx
+      integer                 :: loc_idx, n_hall, k_idx
       integer                 :: ierr(2), all_err(1)
 
       all_err = 0
@@ -1289,7 +1288,7 @@ contains
       integer   , intent(in)  :: kidx_all(:)
       real(8), intent(in)     :: omega_z_all(:,:)
       real(8), allocatable    :: hall(:)
-      integer                 :: loc_idx, n, m, n_hall, k_idx
+      integer                 :: loc_idx, n_hall, k_idx
       integer                 :: ierr(2), all_err(1)
 
       all_err = 0
