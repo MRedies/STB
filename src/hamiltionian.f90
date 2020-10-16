0e30bad5d19cd13976a1d5fe2d730da507f17c88
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-10-16 14:19:36 +0200

    give spherical coordinates for axis, not cartesian

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1322,7 +1322,6 @@ contains
                  c_1, self%del_H, n_dim,&
                  eig_vec_mtx, n_dim,&
                  c_0, tmp, n_dim)
-      deallocate(self%del_H)
       if(allocated(ret))then
          if(size(ret,1) /= n_dim .or. size(ret,2) /= n_dim) then
             deallocate(ret)
@@ -1330,6 +1329,7 @@ contains
       endif
       if(.not. allocated(ret)) allocate(ret(n_dim, n_dim),stat=ierr(3))
       call check_ierr(ierr, me_in=self%me, msg=["failed alloc in calc_velo_mtx"])
+      deallocate(self%del_H)
       ret=(0d0,0d0)
       call zgemm('C', 'N', n_dim, n_dim, n_dim, &
                  c_1, eig_vec_mtx, n_dim, &
