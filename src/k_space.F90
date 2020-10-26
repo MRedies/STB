347952f1eb8e8c23bbe2fcdb00e900c99ac97ff3
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-10-26 11:35:21 +0100

    gather berry

--- src/k_space.F90
+++ src/k_space.F90
@@ -1070,7 +1070,7 @@ contains
             call sections(self%nProcs, send_count*self%nProcs, num_elems, offsets)
             num_elems =  num_elems
             offsets   =  offsets
-            write(*,*) "OMEGA: ", size(var_send), size(send_count), size(var_all_all), size(num_elems), size(offsets)
+            write(*,*) "OMEGA: ", size(var_send), send_count, size(var_all_all), size(num_elems), size(offsets)
             call MPI_Gatherv(var_send, send_count, MPI_REAL8, &
                            var_all_all,     num_elems,  offsets,   MPI_REAL8,&
                            root,        MPI_COMM_WORLD, ierr)
