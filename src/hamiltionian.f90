5cf232b22931af616301283ae3f2b0163e1882e7
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-11-16 15:08:47 +0100

    E_fermi

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1491,7 +1491,7 @@ contains
          do n = 1,n_dim
             do m = 1,n_dim
                !if(n /= m) then
-                  ferm  =  self%fermi_distr(eig_val(n), n_fermi)
+                  ferm  =  self%fermi_distr(eig_val(n), n_fermi) * self%fermi_distr(eig_val(m), n_fermi)
                   call self%calc_fac_diag(eig_val(n), eig_val(m), self%E_fermi(n_fermi),fac)
                   z_comp(n_fermi) = z_comp(n_fermi) + 1d0/(Pi) *&
                               ferm * fac * real(x_mtx(n,m) * x_mtx(m,n))
