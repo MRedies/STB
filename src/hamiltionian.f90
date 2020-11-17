9eb293c262f46bed716ae3f772b7a722f67b0389
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-11-17 09:02:41 +0100

    sea surf diag terms

--- src/hamiltionian.f90
+++ src/hamiltionian.f90
@@ -1494,7 +1494,7 @@ contains
                   ferm  =  1d0!self%fermi_distr(eig_val(n), n_fermi) * self%fermi_distr(eig_val(m), n_fermi)
                   call self%calc_fac_diag(eig_val(n), eig_val(m), self%E_fermi(n_fermi),fac)
                   z_comp(n_fermi) = z_comp(n_fermi) + 1d0/(Pi) *&
-                              ferm * fac * aimag(x_mtx(n,m) * x_mtx(m,n))
+                              ferm * fac * real(x_mtx(n,m) * x_mtx(m,n))
                !endif
             enddo
          enddo
