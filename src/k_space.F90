00f950bb2d2a6193790837abcf5d1b4f6b2674ed
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2020-11-24 15:08:35 +0100

    sea surf diag terms

--- src/k_space.F90
+++ src/k_space.F90
@@ -1068,7 +1068,7 @@ contains
             cnt = cnt + 1
          enddo
       else if(.not. pert_log) then
-         write(*,*) "PROC:",self%me,self%berry_component, norm2(omega_sea_new(:,1))
+         write(*,*) "PROC:",self%me,self%berry_component,trim(self%berry_component) ,norm2(omega_sea_new(:,1)), self%calc_hall_diag
          do k_idx = first, last
             k = self%new_k_pts(:,k_idx)
             call self%ham%calc_eig_and_velo(k, eig_val_new(:,cnt), del_kx, del_ky,0)
