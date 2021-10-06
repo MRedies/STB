80b7c2ae42d02d37c730a2f72bb3b1d4aaa7af90
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2021-10-06 14:11:55 +0200

    set seed and broadcast to comm

--- src/main.f90
+++ src/main.f90
@@ -36,7 +36,7 @@ program STB
    call determine_color(n_sample_par,nProcs,me,color)
 
    key = me!sorting in new comm according to rank in world
-   call MPI_Comm_Split(MPI_COMM_WORLD, color, key, sample_comm)
+   call MPI_Comm_split(MPI_COMM_WORLD, color, key, sample_comm)
    call MPI_Comm_rank(sample_comm, me_sample, ierr)
    if(me_sample==root) then
       call random_seed(size = seed_sz)
