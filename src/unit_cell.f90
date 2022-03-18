7736710c62c0eb5324fe6aeaeaa44feb45301eba
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2022-03-18 15:11:30 +0100

    finding all neighbors

--- src/unit_cell.f90
+++ src/unit_cell.f90
@@ -481,6 +481,7 @@ contains
       self%lattice(:, 2) = self%lattice_constant*transl_mtx(2, :)
 
       call self%setup_gen_conn(conn_mtx, [nn_conn, nn_conn, nn_conn], transl_mtx)
+      call self%set_honey_snd_nearest()
       deallocate (m, pos)
    end subroutine init_file_honey
 
