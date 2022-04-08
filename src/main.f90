770dacf619d70f69628f0c7e819716ce4fe1481b
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2022-04-08 13:20:24 +0200

    correct calc_color

--- src/main.f90
+++ src/main.f90
@@ -373,6 +373,9 @@ contains
       color = 0
       if (nProcs>=2*min_comm_size) then
          color = mod(rank,ncomms)
+         if (color = 0) then
+            color = rank/ncomms
+         endif
       endif
 
    end subroutine
