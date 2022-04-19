9e61dd3a8a0d82c9960c15d35cedcd0154d5d2a3
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2022-04-19 15:37:39 +0200

    append functions

--- src/main.f90
+++ src/main.f90
@@ -66,7 +66,7 @@ program STB
       startidx = calc_starting_sample(n_sample_par,ncomms,color)
       stopidx = startidx + samples_per_comm - 1
       do n_sample = color+1,n_sample_par,ncomms
-         call add_to_arr1D(sample_arr,n_sample)
+         call add_to_arr1D_int(sample_arr,n_sample)
          call process_file(inp_files(1),sample_comm,n_sample,samples_per_comm)
       enddo
    else
