fbda7590e5fa81df34b08aaf1febcff5c58e151e
j.kipp@fz-juelich.de <j.kipp@fz-juelich.de>
2022-07-12 15:15:54 +0200

    when communicating, specify bounds to avoid bugs with gfortran builds

--- src/unit_cell.f90
+++ src/unit_cell.f90
@@ -321,7 +321,9 @@ contains
       call MPI_Bcast(self%dblatan_pref, 1, MPI_REAL8, &
                      root, self%sample_comm, ierr(24))
       call MPI_Bcast(self%atan_pref, 1, MPI_REAL8, root, self%sample_comm, ierr(25))
-
+      if (self%me_sample== root) then
+         write(*,*) "IN BCAST UC SUCCESS"
+      endif
        !BCAST FILES, SINCE IN EVERY SUBCOMM THE ROOT NEEDS TO READ
       call MPI_Bcast(self%mag_file, 300, MPI_CHARACTER, &
                      root, self%sample_comm, ierr(27))
